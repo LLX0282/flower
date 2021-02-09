34847dae577e5e915f886400b7f9c77b724841ea
liulinxin <1752199721@qq.com>
2021-02-09 21:31:21 +0800

    适配移动端 px 转rem

--- flower-app/src/App.vue
+++ flower-app/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+ 
+}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, /* structural elements 结构元素 */
+dl, dt, dd, ul, ol, li, /* list elements 列表元素 */
+pre, /* text formatting elements 文本格式元素 */
+form, fieldset, legend, button, input, textarea, /* form elements 表单元素 */
+th, td /* table elements 表格元素 */ {
+    margin: 0;
+    padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+body, ul, li, h1, h2, h3, h4, h5, h6, p, form, dl, dt, dd { 
+    margin: 0px; 
+    padding: 0px; 
+     
+    font-weight: normal; }
+ul { list-style: none; }
 </style>
--- flower-app/src/views/Home.vue
+++ flower-app/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    123
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //HelloWorld
   }
 };
 </script>
+<style lang='scss' scoped>
+.home{
+   width: 750px;
+    height: 500px;
+    font-size: 12px;
+  background-color: red;
+}
+</style>
